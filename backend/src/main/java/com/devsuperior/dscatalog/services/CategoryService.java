a82974487eec88fef5b8d3d0cda372b902dc8ae5
Diego Carvalho de Freitas <diegocar.freitas@hotmail.com>
2021-09-28 08:47:37 -0300

    Busca categoria por id e adiciona renderização customizada para exceções

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -3,11 +3,13 @@ package com.devsuperior.dscatalog.services;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,5 +24,12 @@ public class CategoryService {
         return categories.stream()
                 .map(category -> new CategoryDTO(category))
                 .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> optional = repository.findById(id);
+        Category category = optional.orElseThrow(() -> new EntityNotFoundException("Categoria não encontrada"));
+        return new CategoryDTO(category);
     }
 }
