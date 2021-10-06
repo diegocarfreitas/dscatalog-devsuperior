bdcfe03653d0e3e65f8d49b80a709651b394a99d
Diego Carvalho de Freitas <diegocar.freitas@hotmail.com>
2021-10-06 06:09:31 -0300

    Mapeamento de entidades e inclusão de dados para teste no banco de dados

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -25,6 +27,9 @@ public class Category implements Serializable {
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant updatedAt;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category(String name) {
         this.name = name;
---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Role.java
@@ -0,0 +1,21 @@
+package com.devsuperior.dscatalog.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_role")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+}
