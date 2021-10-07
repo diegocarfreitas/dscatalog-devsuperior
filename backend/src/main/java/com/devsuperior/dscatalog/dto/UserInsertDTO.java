7dacae1916bcc211cfd3602dec8716bc62e381d2
Diego Carvalho de Freitas <diegocar.freitas@hotmail.com>
2021-10-07 06:41:40 -0300

    Adiciona validação do atualização do usuário

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -1,12 +1,8 @@
 package com.devsuperior.dscatalog.dto;
 
 import com.devsuperior.dscatalog.services.validation.UserInsertValid;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Data
 @UserInsertValid
 public class UserInsertDTO extends UserDTO {
---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/validation/UserUpdateValidator.java
@@ -0,0 +1,50 @@
+package com.devsuperior.dscatalog.services.validation;
+
+import com.devsuperior.dscatalog.dto.UserUpdateDTO;
+import com.devsuperior.dscatalog.entities.User;
+import com.devsuperior.dscatalog.repositories.UserRepository;
+import com.devsuperior.dscatalog.resources.exceptions.FieldMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserUpdateDTO> {
+
+    @Autowired
+    HttpServletRequest request;
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void initialize(UserUpdateValid ann) {
+    }
+
+    @Override
+    public boolean isValid(UserUpdateDTO userUpdateDTO, ConstraintValidatorContext context) {
+        var uriVars = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        long userId = Long.parseLong(uriVars.get("id"));
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        User user = repository.findByEmail(userUpdateDTO.getEmail());
+        if (user != null && user.getId() != userId) {
+            list.add(new FieldMessage("email", "O email indicado já existe"));
+        }
+
+        for (FieldMessage fieldMessage : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(fieldMessage.getMessage())
+                    .addPropertyNode(fieldMessage.getFieldName())
+                    .addConstraintViolation();
+        }
+
+        return list.isEmpty();
+    }
+}
